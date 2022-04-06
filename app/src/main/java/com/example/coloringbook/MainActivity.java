45076b359b918f0e9fa9177470346712460e68ba
brusdamson@gmail.com <brusdamson@gmail.com>
2022-04-07 00:41:06 +0300

    Рабочие картинки

--- app/src/main/java/com/example/coloringbook/MainActivity.java
+++ app/src/main/java/com/example/coloringbook/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.coloringbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParseException;
+import com.larvalabs.svgandroid.SVGParser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
