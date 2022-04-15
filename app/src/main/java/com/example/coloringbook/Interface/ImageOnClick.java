774eb9c0be2e5bb48036d92c1912ced428081906
brusdamson@gmail.com <brusdamson@gmail.com>
2022-04-15 23:22:52 +0300

    Сделал заливку. Фиксанул координаты. Спиздил палитру. Репнул араба

---
+++ app/src/main/java/com/example/coloringbook/Interface/ImageOnClick.java
@@ -0,0 +1,5 @@
+package com.example.coloringbook.Interface;
+
+public interface ImageOnClick {
+    void OnClick(int pos);
+}
---
+++ app/src/main/java/com/example/coloringbook/common/Common.java
@@ -0,0 +1,8 @@
+package com.example.coloringbook.common;
+
+import android.graphics.Color;
+
+public class Common {
+    public static int PICTURE_SELECTED;
+    public static int COLOR_SELECTED = Color.RED;
+}
